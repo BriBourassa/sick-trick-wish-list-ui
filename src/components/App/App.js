a951bc30433422113a08c8c0dfc3814e8e3ecb9a
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-04-10 09:44:17 -0600

    Add Router, set up rudimentary fetch, create skeleton Component layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,34 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+
+    }
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:3001/api/v1/tricks")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
   render() {
+
+
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+
+        
       </div>
     );
   }
-}
+
+
+};
 
 export default App;
---
+++ src/components/TricksList/TricksList.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const TricksList = () => {
+    
+
+};
+
+export default TricksList
